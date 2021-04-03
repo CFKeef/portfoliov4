5587aeff61cbf4516c0d192f82041f9b2748574a
Patryck <PatryckG@outlook.com>
2021-04-03 10:02:36 -0400

    Update index.ts

--- components/general/input/index.ts
+++ components/general/input/index.ts
@@ -4,7 +4,7 @@ export const Button = styled.button `
 	height: 2.2rem;
 	width: 100%;
 	background-color: var(--purple);
-	border: 1px solid var(--purple);
+	border: 2px solid var(--purple);
 	font-weight: bold;
 	border-radius: var(--border-radius);
 	transition: background-color 0.15s ease;
